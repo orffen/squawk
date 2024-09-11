b20cb09485224c309b1c848fb62d2d3cd41fc90f
Orffen <orffen@orffenspace.com>
2024-09-11 14:53:41 +1000

    TOD calculator completed

--- ui.js
+++ ui.js
@@ -5,3 +5,16 @@ function squawkCodeButton() {
 }
 
 squawkCodeButton();
+
+function updateTOD() {
+    let tod_values = {
+        angle: Number(document.getElementById("todAngle").value),
+        ground_speed: Number(document.getElementById("todGroundSpeed").value),
+        current_altitude: Number(document.getElementById("todCurrentAlt").value),
+        target_altitude: Number(document.getElementById("todTargetAlt").value)
+    };
+    let fpm = tod_calc_rate(tod_values.ground_speed, tod_values.angle);
+    let distance = tod_calc_distance(tod_values.current_altitude, tod_values.target_altitude, tod_values.angle);
+    fpm > 0 ? document.getElementById("todDescentRate").innerText = `${fpm.toFixed(0)} fpm` : document.getElementById("todDescentRate").innerHTML = "&nbsp;";
+    distance > 0 ? document.getElementById("todDistanceReq").innerText = `${distance.toFixed(1)} nm` : document.getElementById("todDistanceReq").innerHTML = "&nbsp;";
+}
